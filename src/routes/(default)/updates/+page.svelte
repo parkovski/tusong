0c1df571a1459306bf93a9f010b74bd231b5ef3b
Parker Snell <parkersnell@gmail.com>
2023-10-24 13:54:24 -0700

    Add date to updates listings

--- src/routes/(default)/updates/+page.svelte
+++ src/routes/(default)/updates/+page.svelte
@@ -1,6 +1,25 @@
 <script>
   export let data;
+
+  function formatDate(date) {
+    const dateParts = date.split('-').map(x => +x);
+    return new Date(dateParts[0], dateParts[1]-1, dateParts[2]).toDateString();
+  }
 </script>
+
+<style>
+  ul {
+    padding-left: 0;
+  }
+  li {
+    line-height: 150%;
+    list-style: none;
+  }
+  .date {
+    font-size: 100%;
+    font-style: italic;
+  }
+</style>
 
 <svelte:head>
   <title>Tu Song - Updates</title>
@@ -10,6 +29,9 @@
 
 <ul>
   {#each data.posts as post}
-    <li><a href={post.path}>{post.meta.title}</a></li>
+    <li>
+      <a href={post.path}>{post.meta.title}
+        (<span class="date">{formatDate(post.meta.date)}</span>)</a>
+    </li>
   {/each}
 </ul>
